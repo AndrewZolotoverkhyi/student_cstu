4330d14fbd5d562128caf8400e86fb74df940725
AndrewZolotoverkhyi <a-zolotoverhii@ukr.net>
2020-01-21 13:12:38 +0200

    initial project setup and initial interface elements

--- app/src/main/java/com/example/student_cstu/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/student_cstu/ui/home/HomeFragment.java
@@ -1,9 +1,11 @@
 package com.example.student_cstu.ui.home;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -23,13 +25,8 @@ public class HomeFragment extends Fragment {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
         return root;
+
     }
 }
